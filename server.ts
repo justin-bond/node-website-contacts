93702f1b84188b7e32fe0734803295aaef8127e3
Justin Bond <bond.justink@gmail.com>
2023-08-28 14:29:42 -0700

    add sentry

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import * as Sentry from "@sentry/node";
 
 dotenv.config();
 
@@ -10,10 +11,35 @@ const config = require("./config.ts");
 const indexRouter = require("./routes");
 const router = express.Router();
 
+Sentry.init({
+  dsn: process.env.PUBLIC_SENTRY_DSN,
+  environment: process.env.NODE_ENV,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({
+      tracing: true,
+    }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({
+      app,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!,
+});
+
+// Trace incoming requests
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 router.use("/", indexRouter);
 
 app.use(bodyParser.json());
 app.use(config.baseUrl, router);
+
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
 app.use(function (req, res) {
   res.status(404).send({ error: `url ${req.originalUrl} not found` });
 });
